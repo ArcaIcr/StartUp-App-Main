b411a1730ad3420c94a3916c0df0b7c6b6f28e69
Nyxon <adriane.loquinte@gmail.com>
2024-12-21 02:59:02 +0800

    chore: slight cleanup + added trends functionality

--- src/views/DashboardViews/DashboardOverview.vue
+++ src/views/DashboardViews/DashboardOverview.vue
@@ -6,8 +6,6 @@
       <StatsCards />
       <div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 p-4">
         <TrendChart />
-        <SalesChart />
-        <TransactionsChart class="lg:col-span-2 xl:col-span-3" />
       </div>
     </div>
   </div>
@@ -19,8 +17,6 @@ import Sidebar from "@/components/dashboard/Sidebar.vue";
 import Header from "@/components/dashboard/Header.vue";
 import StatsCards from "@/components/dashboard/Cards/StatCard.vue";
 import TrendChart from "@/components/dashboard/Charts/TrendChart.vue";
-import SalesChart from "@/components/dashboard/Charts/SalesChart.vue";
-import TransactionsChart from "@/components/dashboard/Charts/TransactionsChart.vue";
 
 // Commented out for future use
 // import GoalsChart from "@/components/dashboard/Charts/GoalsChart.vue";
